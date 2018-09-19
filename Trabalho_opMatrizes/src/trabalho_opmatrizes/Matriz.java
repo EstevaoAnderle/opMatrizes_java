3240d2b9d0cc0671e3f392025daa1b2f6354f8d0
EstevaoAnderle <estevao.anderle@gmail.com>
2018-09-18 23:05:16 -0300

    Reestruturação do projeto e melhor divisão nas classes.

--- Trabalho_opMatrizes/src/trabalho_opmatrizes/Matriz.java
+++ Trabalho_opMatrizes/src/trabalho_opmatrizes/Matriz.java
@@ -10,20 +10,42 @@ package trabalho_opmatrizes;
  * @author estev
  */
 public class Matriz {
-  
-  public int traduzirNumeralRomano1(String texto) {
-		int n = 0;
-		int numeralDaDireita = 0;
-		for (int i = texto.length() - 1; i >= 0; i--) {
-			int valor = (int) traduzirNumeralRomano(texto.charAt(i));
-			n += valor * Math.signum(valor + 0.5 - numeralDaDireita);
-			numeralDaDireita = valor;
-		}
-		return n;
-	}
-  
-	public double traduzirNumeralRomano(char caractere) {
-		return Math.floor(Math.pow(10, "IXCM".indexOf(caractere))) + 5 * Math.floor(Math.pow(10, "VLD".indexOf(caractere)));
-	}
-  
+
+    String vetorEnt;
+    //String vetorEnt2;
+    String[] vetor;
+    String[][] matriz = new String[vetor.length][];
+    Integer[][] matrizConvertida = new Integer[vetor.length][];
+
+    public String[] stringParaVetor(String entrada) {
+        entrada = entrada.replace("[", "");
+        entrada = entrada.replace("]", "");
+
+        this.vetor = entrada.split("\\s*,+\\s*");
+
+        return vetor;
+    }
+
+    public String[][] vetorParaMatriz(String[] vetor) {
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i] = vetor[i].split("\\s+");
+        }
+
+        return matriz;
+    }
+
+    public int traduzirNumeralRomano1(String texto) {
+        int n = 0;
+        int numeralDaDireita = 0;
+        for (int i = texto.length() - 1; i >= 0; i--) {
+            int valor = (int) traduzirNumeralRomano(texto.charAt(i));
+            n += valor * Math.signum(valor + 0.5 - numeralDaDireita);
+            numeralDaDireita = valor;
+        }
+        return n;
+    }
+
+    public double traduzirNumeralRomano(char caractere) {
+        return Math.floor(Math.pow(10, "IXCM".indexOf(caractere))) + 5 * Math.floor(Math.pow(10, "VLD".indexOf(caractere)));
+    }
 }
--- Trabalho_opMatrizes/src/trabalho_opmatrizes/Trabalho_opMatrizes.java
+++ Trabalho_opMatrizes/src/trabalho_opmatrizes/Trabalho_opMatrizes.java
@@ -20,73 +20,111 @@ public class Trabalho_opMatrizes {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        Matriz matrizConvert = new Matriz();
+        Matriz matrizConvert2 = new Matriz();
 
         //System.out.println("Informe a primeira matriz, com base no seguinte exemplo:");
         //Inserir descritivo
         //matriz1 = scan.nextLine();
         //Entender porque não funciona com o next.
-        //String[] arrayVetor = matriz1.split("\\s");
-        //String[][] matrizVetor = arrayVetor.split(",");
-        //System.out.println(Arrays.toString(arrayVetor));
         System.out.println("Informe a primeira matriz:");
         String vetorEnt = scan.nextLine();
-        vetorEnt = vetorEnt.replace("[", "");
-        vetorEnt = vetorEnt.replace("]", "");
-        //adicionar explicação
-        //adicionar validação por ponto
+        matrizConvert.stringParaVetor(vetorEnt);
+        //vetorEnt = vetorEnt.replace("[", "");
+        //vetorEnt = vetorEnt.replace("]", "");
 
-        String[] vetor1 = vetorEnt.split("\\s*,+\\s*");
-        System.out.println("Vetor quebrado" + Arrays.toString(vetor1));
+        //String[] vetor1 = vetorEnt.split("\\s*,+\\s*");
+        System.out.println("Vetor quebrado da primeira matriz" + Arrays.toString(matrizConvert.stringParaVetor(vetorEnt)));
 
-        String[][] matriz1 = new String[vetor1.length][];
-        Integer[][] matrizConvertida = new Integer[vetor1.length][];
+        System.out.println("Informe a segunda matriz:");
+        String vetorEnt2 = scan.nextLine();
+        matrizConvert2.stringParaVetor(vetorEnt2);
+
+        //String[] vetor2 = vetorEnt2.split("\\s*,+\\s*");
+        System.out.println("Vetor quebrado da segunda matriz" + Arrays.toString(matrizConvert2.stringParaVetor(vetorEnt)));
+
+        //String[][] matriz1 = new String[matrizConvert.vetor.length][];
+        //Integer[][] matrizConvertida1 = new Integer[matrizConvert.vetor.length][];
+        // String[][] matriz2 = new String[matrizConvert2.vetor.length][];
+        //Integer[][] matrizConvertida2 = new Integer[matrizConvert2.vetor.length][];
         //linha e depois coluna
+        //*********************Conversão da primeira matriz
+//        for (int i = 0; i < matriz1.length; i++) {
+//            matriz1[i] = matrizConvert.vetor[i].split("\\s+");
+//        }
+        matrizConvert.vetorParaMatriz(matrizConvert.vetor);
 
-        //matrizConvertida[0][0] = 0;
-        for (int i = 0; i < matriz1.length; i++) {
-            matriz1[i] = vetor1[i].split("\\s+");
-        }
+        //*********************Conversão da segunda matriz
+//        for (int i = 0; i < matriz2.length; i++) {
+//            matriz2[i] = matrizConvert2.vetor[i].split("\\s+");
+//        }
+        matrizConvert2.vetorParaMatriz(matrizConvert2.vetor);
 
+        //Conversão para romano da primeira matriz
         int convertido = 0;
-        Matriz matriz = new Matriz();
 
-        for (int i = 0; i < matriz1.length; i++) {
+        for (int i = 0; i < matrizConvert.matriz.length; i++) {
             //inicilização da matriz
             //aqui está dizendo quantas colunas tem em cada linha
-            matrizConvertida[i] = new Integer[matriz1[i].length];
+            matrizConvert.matrizConvertida[i] = new Integer[matrizConvert.matriz[i].length];
 
-            for (int j = 0; j < matriz1[i].length; j++) {
-                //System.out.print(matriz1[i][j] + "");
-                System.out.print(matriz1[i][j] + "|");
+            for (int j = 0; j < matrizConvert.matriz[i].length; j++) {
+                System.out.print(matrizConvert.matriz[i][j] + "|");
 
-                convertido = matriz.traduzirNumeralRomano1(matriz1[i][j]);
-                //matriz1[i][j] = Integer.toString(convertido);
-                //matrizConvertida[i][j] = Integer.parseInt( matriz1[i][j]);
-                matrizConvertida[i][j] = convertido;
+                convertido = matrizConvert.traduzirNumeralRomano1(matrizConvert.matriz[i][j]);
+                matrizConvert.matrizConvertida[i][j] = convertido;
             }
             System.out.println();
         }
 
-        System.out.println("Conversão:");
-        for (int i = 0; i < matrizConvertida.length; i++) {
-            for (int j = 0; j < matrizConvertida[i].length; j++) {
-                //System.out.print(matriz1[i][j] + "");
-                System.out.print(matrizConvertida[i][j] + "|");
+        //Conversão para romano da segunda matriz
+        int convertido2 = 0;
+        for (int i = 0; i < matrizConvert2.matriz.length; i++) {
+            //inicilização da matriz
+            //aqui está dizendo quantas colunas tem em cada linha
+            matrizConvert2.matrizConvertida[i] = new Integer[matrizConvert2.matriz[i].length];
+
+            for (int j = 0; j < matrizConvert2.matriz[i].length; j++) {
+                System.out.print(matrizConvert2.matriz[i][j] + "|");
+
+                convertido2 = matrizConvert2.traduzirNumeralRomano1(matrizConvert2.matriz[i][j]);
+                matrizConvert2.matrizConvertida[i][j] = convertido2;
             }
             System.out.println();
         }
 
-        System.out.println(matrizConvertida.length + " X " + matrizConvertida[1].length);
+        //Escrita da matriz 1 convertida
+        System.out.println("Conversão da primeira matriz:");
+        for (int i = 0; i < matrizConvert.matrizConvertida.length; i++) {
+            for (int j = 0; j < matrizConvert.matrizConvertida[i].length; j++) {
+                //System.out.print(matriz1[i][j] + "");
+                System.out.print(matrizConvert.matrizConvertida[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+        System.out.println(matrizConvert.matrizConvertida.length + " X " + matrizConvert.matrizConvertida[0].length);
+
+        //Escrita da matriz 2 convertida
+        System.out.println("Conversão da segunda matriz:");
+        for (int i = 0; i < matrizConvert2.matrizConvertida.length; i++) {
+            for (int j = 0; j < matrizConvert2.matrizConvertida[i].length; j++) {
+                //System.out.print(matriz1[i][j] + "");
+                System.out.print(matrizConvert2.matrizConvertida[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+        System.out.println(matrizConvert2.matrizConvertida.length + " X " + matrizConvert2.matrizConvertida[0].length);
 
         //adicionar verificação para matriz de uma linha só
-        for (int i = 0; i < matriz1.length - 1; i++) {
-
-            if (matriz1[i].length != matriz1[(i + 1)].length) {
-                System.out.println("Matriz inválida. Tente novamente!");
-                break;
-            }
-        }
-
+//        for (int i = 0; i < matrizConvert.matriz.length - 1; i++) {
+//
+//            if (matrizConvert.matriz[i].length != matrizConvert.matriz[(i + 1)].length) {
+//                System.out.println("Matriz inválida. Tente novamente!");
+//                break;
+//            }
+//        }
         //http://www.guj.com.br/t/metodo-para-converter-numeros-romanos-para-decimal-alguem-sabe/81923/2
         //http://www.guj.com.br/t/como-converter-inteiro-para-string/24091
     }
